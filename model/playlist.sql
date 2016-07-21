80ea96d07fdb4faabe23b53d6b6b599a147619ca
Constantin <cdval13@freeuni.edu.ge>
2016-07-21 21:14:02 +0400

    Changed CHAR to VARCHAR in some places where the static string length was not needed

--- model/playlist.sql
+++ model/playlist.sql
@@ -1,17 +1,17 @@
 CREATE TABLE users(
 	user_id SERIAL PRIMARY KEY,
-	username CHAR(50) NOT NULL UNIQUE,
-	password CHAR(64) NOT NULL,
-	name CHAR(30) NOT NULL,
+	username varchar(50) NOT NULL UNIQUE,
+	password varchar(64) NOT NULL,
+	name varchar(30) NOT NULL,
 	is_admin BOOLEAN NOT NULL DEFAULT FALSE,
 	token CHAR(30),
-	token_date TIMESTAMP
+	token_date REAL DEFAULT EXTRACT(EPOCH FROM now())
 );
 
 CREATE TABLE places(
 	place_id SERIAL PRIMARY KEY,
 	user_id INT NOT NULL,
-	place_name CHAR(100) NOT NULL,
+	place_name varchar(100) NOT NULL,
 	latitude REAL NOT NULL,
 	longitude REAL NOT NULL,
 	FOREIGN KEY (user_id)  REFERENCES users(user_id)
@@ -21,7 +21,7 @@ CREATE TABLE songs(
 	song_id SERIAL PRIMARY KEY,
 	place_id INT NOT NULL,
 	user_id INT NOT NULL,
-	song_name CHAR(50),
+	song_name varchar(50),
 	FOREIGN KEY (place_id) REFERENCES places(place_id),
 	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
